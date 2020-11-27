ab5ef1defe1738df9f78e84e5316ce297da0d43b
Brett Leedy <bmleedy@fb.com>
2020-11-27 14:56:01 -0800

    get linter working with custom-build cmake in travis.

--- run_lint.bash
+++ run_lint.bash
@@ -4,9 +4,13 @@
 echo "run_lint.bash: LINTING START=========================================================="
 
 
-cpplint --recursive --exclude=test/googletest/ --exclude=test/build  ./* 2>&1
+cpplint --recursive --exclude=test/googletest/ --exclude=deps/ --exclude=test/build  ./* 2>&1
 
-MESSAGE=`cpplint --recursive --exclude=*.gitignore --exclude=test/googletest/ --exclude=test/build 2>&1 ./* | grep "Total errors found"`
+MESSAGE=`cpplint --recursive --exclude=deps/ \
+         --exclude=*.gitignore \
+         --exclude=test/googletest/ \
+         --exclude=test/build \
+         2>&1 ./* | grep "Total errors found"`
 
 if [ "${MESSAGE}" = "" ]; then
     echo "run_lint.bash: ======================================================================="
