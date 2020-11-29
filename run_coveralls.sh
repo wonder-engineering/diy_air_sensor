4fc96d0e009123839b1b177f0bf3e4aebffe3b08
Brett Leedy <bmleedy@gmail.com>
2020-11-29 10:33:41 -0800

    verbosity added

--- run_coveralls.sh
+++ run_coveralls.sh
@@ -1,5 +1,5 @@
 COVERALLS_REPO_TOKEN=CteyznTyrfm3aLBu5XefQ1fU9K5fqFoZ1 \
-cpp-coveralls --skip-ssl-verify\
+cpp-coveralls -v --skip-ssl-verify\
     --skip-ssl-verify \
     --exclude test/googletest \
     --exclude test/build \
