304f577afbac0f9b111a7ead314f93519b35a461
Brett Leedy <bmleedy@gmail.com>
2020-11-29 09:39:37 -0800

    added script to separately run coveralls

--- run_coveralls.sh
+++ run_coveralls.sh
@@ -1,3 +1,7 @@
 COVERALLS_REPO_TOKEN=CteyznTyrfm3aLBu5XefQ1fU9K5fqFoZ1 \
 cpp-coveralls \
-    --skip-ssl-verify
+    --skip-ssl-verify \
+    --exclude test/googletest \
+    --exclude test/build \
+    --exclude build \
+    --exclude cmake-modules
