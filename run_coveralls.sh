ee87104ab0c803864d48bd4666c70094adc24212
Brett Leedy <bmleedy@gmail.com>
2020-11-29 10:28:15 -0800

    debugging ssl issue with coveralls

--- run_coveralls.sh
+++ run_coveralls.sh
@@ -1,7 +1,8 @@
 COVERALLS_REPO_TOKEN=CteyznTyrfm3aLBu5XefQ1fU9K5fqFoZ1 \
-cpp-coveralls \
+cpp-coveralls --skip-ssl-verify\
     --skip-ssl-verify \
     --exclude test/googletest \
     --exclude test/build \
     --exclude build \
-    --exclude cmake-modules
+    --exclude cmake-modules --skip-ssl-verify
+cpp-coveralls -h
